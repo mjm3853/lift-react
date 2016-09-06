4e985fe45011190e2df3232712f140a56d32992b
Matt McGrath <mjm3853@yahoo.com>
2016-09-05 21:41:16 -0400

    form skeleton

--- src/components/AddLift/AddLift.js
+++ src/components/AddLift/AddLift.js
@@ -7,6 +7,28 @@ class AddLift extends Component {
                 <div className="panel-heading">
                     <h2>Add Lift</h2>
                 </div>
+                <div className="panel-body">
+                    <form>
+                        <div className="form-group">
+                            <input className="form-control" type="text" placeholder="Workout Name" />
+                        </div>
+                        <div className="well well-sm">
+                            <div className="form-group">
+                                <input className="form-control" type="text" placeholder="Lift Name" />
+                            </div>
+                            <div className="well well-sm">
+                                <div>
+                                    <p>Sets</p>
+                                    <input className="form-control" type="text" placeholder="Reps" />
+                                    <input className="form-control" type="text" placeholder="Weight" />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <input className="form-control btn btn-success" type="submit" value="Add Lift!" />
+                        </div>
+                    </form>
+                </div>
             </div>
         );
     }
